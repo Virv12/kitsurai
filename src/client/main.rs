874b91ae495da3bd3334b496b36518737135d114
Jacopo Del Granchio <jacopo@delgranchio.it>
2025-03-21 11:07:20 +0100

    Ktc now supports non UTF8 inputs.

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use kitsurai::codec::Header;
+use std::ffi::OsString;
+use std::os::unix::ffi::OsStringExt;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -10,19 +12,21 @@ struct Args {
     key: String,
 
     #[arg(raw = true)]
-    value: Option<String>,
+    value: Option<OsString>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let Args { server, key, value } = Args::parse();
+
     let url = format!("http://{server}/{key}");
+    let client = reqwest::Client::new();
+
     if let Some(value) = value {
-        let client = reqwest::Client::new();
-        let res = client.post(&url).body(value).send().await?;
+        let res = client.post(&url).body(value.into_vec()).send().await?;
         println!("{}: {}", res.status(), res.text().await?.trim_end());
     } else {
-        let res = reqwest::get(url).await?;
+        let res = client.get(&url).send().await?;
         if res.status().is_success() {
             let bytes = res.bytes().await?;
             let (header, rest) = postcard::take_from_bytes::<Header>(&bytes)?;
