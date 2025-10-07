4f7785093fac8bde1d1243c5041b56f7e79a7b78
Filippo Casarin <casarin.filippo17@gmail.com>
2025-10-07 19:22:53 +0200

    Update ktb: using hyper & multi-connection

--- src/bench/main.rs
+++ src/bench/main.rs
@@ -1,57 +1,81 @@
+use anyhow::Result;
+use bytes::Bytes;
 use clap::{Parser, Subcommand};
-use reqwest::{Client, Request};
-use std::ops::Add;
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::{
-    fs::File,
-    io::Write,
-    time::{Duration, Instant},
-};
+use http_body_util::{BodyExt, Full};
+use hyper::client::conn::http2::SendRequest;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use std::sync::Arc;
+use std::{fs::File, io::Write, time::Duration};
+use tokio::net::TcpStream;
 use tokio::task::JoinSet;
+use tokio::time::Instant;
+
+type Client = SendRequest<Full<Bytes>>;
+type Request<T = Full<Bytes>> = http::Request<T>;
+type RequestFn = Arc<dyn Fn(usize) -> Request + Send + Sync>;
 
 type Res = Result<f64, ()>;
 
-async fn exec_req(client: Client, req: Request) -> Res {
+async fn exec_req(mut client: Client, req: Request) -> Res {
     let start_time = Instant::now();
-    let res = client.execute(req).await.map_err(|e| {
+    let res = client.send_request(req).await.map_err(|e| {
         eprintln!("Request failed: {e:?}");
     })?;
     let status = res.status();
-    let _body = res.bytes().await.map_err(|e| {
+    let mut body = res.into_body().map_err(|e| {
         eprintln!("Failed to read response body: {e}");
-    })?;
-    let end_time = Instant::now();
+    });
+    while let Some(chunk) = body.frame().await {
+        chunk.map_err(|e| {
+            eprintln!("Failed to read response body chunk: {e:?}");
+        })?;
+    }
+    let elapsed = start_time.elapsed();
     if !status.is_success() {
         return Err(());
     }
-    Ok(end_time.duration_since(start_time).as_secs_f64() * 1000.)
+    if elapsed > Duration::from_secs(5) {
+        eprintln!("Request took too long: {:?}", elapsed);
+        return Err(());
+    }
+    Ok(elapsed.as_secs_f64() * 1000.)
 }
 
-async fn run_fstress(client: Client, reqs: &[Request], period_us: u64) {
+async fn run_fstress(server: &str, reqs: RequestFn, period_us: u64) {
     let mut set = JoinSet::new();
     let period = Duration::from_micros(period_us);
     let mut interval = tokio::time::interval(period);
 
+    let mut clients = Vec::new();
+    for _ in 0..20 {
+        clients.push(get_conn(server).await.unwrap());
+    }
+
     for i in 0.. {
         while set.try_join_next().is_some() {}
 
-        let req = reqs[i % reqs.len()].try_clone().unwrap();
+        let client = clients[i % clients.len()].clone();
+        let req = reqs(i);
         interval.tick().await;
-        set.spawn(exec_req(client.clone(), req));
+        set.spawn(exec_req(client, req));
     }
 
     set.join_all().await;
 }
 
-async fn run_lstress(client: Client, reqs: &[Request], tasks: u64) {
+async fn run_lstress(server: &str, reqs: RequestFn, tasks: u64) {
     let mut set = JoinSet::new();
+    let mut clients = Vec::new();
+    for _ in 0..20 {
+        clients.push(get_conn(server).await.unwrap());
+    }
 
-    for req in reqs.iter().cycle().take(tasks as usize) {
-        let req = req.try_clone().unwrap();
-        let client = client.clone();
+    for idx in 0..tasks as usize {
+        let reqs = reqs.clone();
+        let client = clients[idx % clients.len()].clone();
         set.spawn(async move {
             loop {
-                let req = req.try_clone().unwrap();
+                let req = reqs(idx);
                 let _ = exec_req(client.clone(), req).await;
             }
         });
@@ -62,36 +86,41 @@ async fn run_lstress(client: Client, reqs: &[Request], tasks: u64) {
 
 async fn bench(
     name: &str,
-    client: Client,
-    reqs: &[Request],
+    server: &str,
+    reqs: RequestFn,
     period: Duration,
     time: Duration,
     out: &mut dyn Write,
 ) -> bool {
-    let mut interval = tokio::time::interval(period);
+    let count_req = (time.as_secs_f64() / period.as_secs_f64()) as usize;
+
+    let mut clients = Vec::new();
+    for _ in 0..20 {
+        clients.push(get_conn(server).await.unwrap());
+    }
+
     let mut set = JoinSet::new();
     let mut res = Vec::new();
     let mut bad_count = 0;
-
-    let count_req = (time.as_secs_f64() / period.as_secs_f64()) as usize;
+    let mut interval = tokio::time::interval(period);
     for idx in 0..count_req {
         while let Some(r) = set.try_join_next() {
             match r.unwrap() {
                 Ok(r) => res.push(r),
                 Err(()) => {
                     bad_count += 1;
-                    if bad_count == 10 {
+                    if bad_count >= 10 {
                         return false;
                     }
                 }
             }
         }
 
+        let client = clients[idx % clients.len()].clone();
+        let req = reqs(idx);
+
         interval.tick().await;
-        set.spawn(exec_req(
-            client.clone(),
-            reqs[idx % reqs.len()].try_clone().unwrap(),
-        ));
+        set.spawn(exec_req(client, req));
 
         if (idx + 1).count_ones() == 1 {
             eprintln!("[{}] {} / {} / {}", name, res.len(), idx + 1, count_req);
@@ -103,7 +132,7 @@ async fn bench(
             Ok(r) => res.push(r),
             Err(()) => {
                 bad_count += 1;
-                if bad_count == 10 {
+                if bad_count >= 10 {
                     return false;
                 }
             }
@@ -136,7 +165,7 @@ async fn bench(
     true
 }
 
-async fn test(name: &str, client: Client, reqs: &[Request]) {
+async fn test(name: &str, server: &str, reqs: RequestFn) {
     let mut out = File::create(format!("bench-{name}.csv")).unwrap();
     writeln!(
         out,
@@ -144,30 +173,24 @@ async fn test(name: &str, client: Client, reqs: &[Request]) {
     )
     .unwrap();
 
-    for req in reqs {
-        exec_req(client.clone(), req.try_clone().unwrap())
-            .await
-            .unwrap();
-    }
+    //const SYNC_S: u64 = 30;
 
-    const SYNC_S: u64 = 25;
-
-    let mut period_us = 200;
+    let mut period_us = 30;
     loop {
-        let unix = UNIX_EPOCH.elapsed().unwrap().as_secs();
-        let unix_start = unix.next_multiple_of(SYNC_S);
-        let unix_diff = UNIX_EPOCH
-            .add(Duration::from_secs(unix_start))
-            .duration_since(SystemTime::now())
-            .unwrap();
-        println!("Starting in {:?}", unix_diff);
-        tokio::time::sleep(unix_diff).await;
+        //let unix = UNIX_EPOCH.elapsed().unwrap().as_secs();
+        //let unix_start = unix.next_multiple_of(SYNC_S);
+        //let unix_diff = UNIX_EPOCH
+        //    .add(Duration::from_secs(unix_start))
+        //    .duration_since(SystemTime::now())
+        //    .unwrap();
+        //println!("Starting in {:?}", unix_diff);
+        //tokio::time::sleep(unix_diff).await;
 
         let period = Duration::from_micros(period_us);
         let good = bench(
             &format!("{name}-{:.3}ms", period_us as f64 / 1000.),
-            client.clone(),
-            reqs,
+            server,
+            reqs.clone(),
             period,
             Duration::from_secs(20),
             &mut out,
@@ -205,6 +228,24 @@ enum Action {
     Set,
 }
 
+async fn get_conn(peer: &str) -> Result<Client> {
+    let stream = TcpStream::connect(peer).await?;
+    let io = TokioIo::new(stream);
+
+    // Perform an HTTP/2 handshake over the TCP connection
+    let (sender, connection) =
+        hyper::client::conn::http2::handshake(TokioExecutor::new(), io).await?;
+
+    // Spawn the connection driver (handles incoming frames)
+    tokio::spawn(async move {
+        if let Err(err) = connection.await {
+            eprintln!("Connection error: {:?}", err);
+        }
+    });
+
+    Ok(sender)
+}
+
 #[tokio::main]
 async fn main() {
     let Args {
@@ -216,47 +257,34 @@ async fn main() {
         action,
     } = Args::parse();
 
-    let client = Client::builder()
-        // Already uses HTTP2 if detected and has nodelay set to true.
-        .http2_prior_knowledge()
-        .build()
-        .unwrap();
-
-    let reqs: Vec<_> = match action {
-        Action::Get => (0..100)
-            .map(|i| {
-                client
-                    .get(format!("http://{server}/{table}/{key}-{i}"))
-                    .timeout(Duration::from_secs(5))
-                    .build()
-                    .unwrap()
-            })
-            .collect(),
-        Action::Set => (0..100)
-            .map(|i| {
-                client
-                    .post(format!("http://{server}/{table}/{key}-{i}"))
-                    .timeout(Duration::from_secs(5))
-                    .body(vec![0u8; 4096])
-                    .build()
-                    .unwrap()
-            })
-            .collect(),
+    let reqs: RequestFn = match action {
+        Action::Get => Arc::new(move |i| {
+            let i = i % 1000;
+            Request::get(format!("/{table}/{key}-{i}"))
+                .body(Full::new(Bytes::new()))
+                .unwrap()
+        }),
+        Action::Set => Arc::new(move |i| {
+            let i = i % 1000;
+            Request::post(format!("/{table}/{key}-{i}"))
+                .body(Full::new(Bytes::from(vec![0u8; 4096])))
+                .unwrap()
+        }),
     };
 
     match (freq_stress, lat_stress) {
         (Some(freq), None) => {
-            run_fstress(client.clone(), &reqs, freq).await;
+            run_fstress(&server, reqs, freq).await;
         }
         (None, Some(lat)) => {
-            run_lstress(client.clone(), &reqs, lat).await;
+            run_lstress(&server, reqs, lat).await;
         }
         (None, None) => {
             let name = match action {
                 Action::Get => "get",
                 Action::Set => "set",
             };
-            test(name, client.clone(), &reqs).await;
+            test(name, &server, reqs).await;
         }
         _ => {
             eprintln!("Please specify either --freq-stress or --lat-stress, not both.");
